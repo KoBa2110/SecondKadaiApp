4354d55aba60d4193ace38e5e86852164a56e4fb
古場 勇人 <kobahayato@koba-hayato-no-macbook-pro.local>
2016-03-03 15:38:03 +0900

    コミットしました。

---
+++ SecondKadaiApp/RisurtViewController.swift
@@ -0,0 +1,35 @@
+//
+//  RisurtViewController.swift
+//  SecondKadaiApp
+//
+//  Created by 古場 勇人 on 2016/03/01.
+//  Copyright © 2016年 古場 勇人. All rights reserved.
+//
+
+import UIKit
+
+class RisurtViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
